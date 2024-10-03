b18f20aaee78b95bb023f2193f9f195e971f5b89
Nick Gannon <nmgannon13@gmail.com>
2024-10-03 14:39:57 -0400

    Working on mobile navbar

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ arrow = document.getElementById('arrow');
 function toggleArrow() {
     arrow.classList.toggle('rotate-180');
 }
+
+main = document.getElementById('main');
+sidebar = document.getElementById('sidebar');
+hamburger = document.getElementById('hamburger');
+hamburger.addEventListener('click', toggleSidebar);
+
+function toggleSidebar() {
+    sidebar.classList.toggle('absolute');
+    sidebar.classList.toggle('hidden');
+    main.classList.toggle('brightness-50');
+}
